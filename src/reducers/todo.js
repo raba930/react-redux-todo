a542eed2d53fc23fbd39479ad372ff506d28222e
Antonio Rabuzin <antonio.rabuzin@dobarkod.hr>
2017-03-06 11:50:20 +0100

    Save todos in localstorage

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,13 +1,16 @@
 import * as constants from '../actions/constants';
 
+let storedTodos = localStorage.todos ? JSON.parse(localStorage.todos).todos : undefined;
+
 const initialState = {
-    todos: []
+    todos: storedTodos || []
 };
 
 export default(state = initialState, action) => {
+    let todos = [];
     switch (action.type) {
     case constants.ADD_TODO:
-        return Object.assign({}, state, {
+        todos = Object.assign({}, state, {
             todos: [
                 ...state.todos,
                 {
@@ -16,12 +19,16 @@ export default(state = initialState, action) => {
                 }
             ]
         });
+        localStorage.setItem('todos', JSON.stringify(todos));
+        return todos;
     case constants.REMOVE_TODO:
-        return Object.assign({}, state, {
+        todos = Object.assign({}, state, {
             todos: state.todos.filter((item, index) => {
                 return index !== action.id;
             })
         });
+        localStorage.setItem('todos', JSON.stringify(todos));
+        return todos;
     default:
         return state;
     }
