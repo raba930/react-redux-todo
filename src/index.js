7513505d3e7d13ad82429605f7ed5825c2f8637b
Antonio Rabuzin <antonio.rabuzin@dobarkod.hr>
2017-03-06 14:11:10 +0100

    Save todos in localStorage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { loadState, saveState } from './localStorage';
 import App from './components/App';
-import './stylesheet/index.css';
 import Store from './store';
+import './stylesheet/index.css';
 import 'materialize-css/bin/materialize.css';
 
-let store = Store();
+const initState = loadState();
+const store = Store(initState);
+
+// TODO: save only todos in ls not complete store
+store.subscribe(() => {
+    saveState(store.getState());
+});
 
 ReactDOM.render(
   <Provider store={store}>
