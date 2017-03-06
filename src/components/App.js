9b9e262fd2c7a2f4a064e9414393e341707725d3
Antonio Rabuzin <antonio.rabuzin@dobarkod.hr>
2017-03-06 16:08:12 +0100

    Added preloader

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,16 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <TodoList />
+                <header className="light-blue accent-3 valign-wrapper">
+                    <h4 className="center-align">REACT-REDUX TODO LIST</h4>
+                </header>
+                <div className="container">
+                    <div className="row">
+                        <div className="col m6 s10 offset-m3 offset-s1">
+                            <TodoList />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
