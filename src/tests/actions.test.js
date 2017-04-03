f311dc3b9462d978a4bcd8aa0f25bbcd41f6b2af
Antonio Rabuzin <antonio.rabuzin@dobarkod.hr>
2017-04-03 16:04:41 +0200

    Added test cases for new actions

--- src/tests/actions.test.js
+++ src/tests/actions.test.js
@@ -19,11 +19,27 @@ describe('actions', () => {
         expect(actions.removeTodo(id)).toEqual(expectedAction);
     });
     it('should create an action to toggle a todo status', () => {
-        const id = 5;
+        const id = 4;
         const expectedAction = {
             type: constants.TOGGLE_COMPLETE,
             id
         };
         expect(actions.toggleComplete(id)).toEqual(expectedAction);
+    });
+    it('should create an action to remove completed todos', () => {
+        const id = 3;
+        const expectedAction = {
+            type: constants.REMOVE_COMPLETE,
+            id
+        };
+        expect(actions.removeCompleted(id)).toEqual(expectedAction);
+    });
+    it('should create an action to set new display filter', () => {
+        const filter = 'SHOW_ACTIVE';
+        const expectedAction = {
+            type: constants.SET_FILTER,
+            filter
+        };
+        expect(actions.setFilter(filter)).toEqual(expectedAction);
     });
 });
