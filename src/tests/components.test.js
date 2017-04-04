36f6e1919bcf92f584815a0dbbcc2138f3f4dcd5
Antonio Rabuzin <antonio.rabuzin@dobarkod.hr>
2017-04-04 17:06:10 +0200

    Added components tests for todo & todos components

--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Todo from '../components/elements/todo';
+import Todos from '../components/elements/todos';
 
-function setup(props) {
+function todoSetup(props) {
     const enzymeWrapper = mount(<Todo {...props} />);
     return {
         props,
         enzymeWrapper
     };
 }
-
+function todosSetup(props) {
+    const enzymeWrapper = mount(<Todos {...props} />);
+    return {
+        props,
+        enzymeWrapper
+    };
+}
 describe('components', () => {
     describe('Todo', () => {
         it('Complete todo item', () => {
@@ -20,7 +27,7 @@ describe('components', () => {
                     completed: true
                 }
             };
-            const { enzymeWrapper } = setup(props);
+            const { enzymeWrapper } = todoSetup(props);
             expect(enzymeWrapper.find('li .todo-text').text()).toEqual('asdf');
             expect(enzymeWrapper.find('li').hasClass('light-green')).toBe(true);
             expect(enzymeWrapper.find('li').is('#5')).toBe(true);
@@ -35,12 +42,43 @@ describe('components', () => {
                     completed: false
                 }
             };
-            const { enzymeWrapper } = setup(props);
+            const { enzymeWrapper } = todoSetup(props);
             expect(enzymeWrapper.find('li .todo-text').text()).toEqual('asdfg');
             expect(enzymeWrapper.find('li').hasClass('light-blue')).toBe(true);
             expect(enzymeWrapper.find('li').is('#4')).toBe(true);
             expect(enzymeWrapper.find('li .controls .complete i').text()).toEqual('done');
             expect(enzymeWrapper.find('li .controls .remove i').text()).toEqual('delete');
+        });
+    });
+    describe('Todos', () => {
+        it('Should render exactly two todo components', () => {
+            const props = {
+                todos: [{
+                    text: 'asdf',
+                    completed: true
+                }, {
+                    text: 'ddf',
+                    completed: false
+                }],
+                filter: 'SHOW_ALL'
+            };
+            const { enzymeWrapper } = todosSetup(props);
+            expect(enzymeWrapper.find('ul').children().nodes.length).toEqual(2);
+        });
+        it('Should render only active todos', () => {
+            const props = {
+                todos: [{
+                    text: 'asdf',
+                    completed: true
+                }, {
+                    text: 'ddf',
+                    completed: false
+                }],
+                filter: 'SHOW_ACTIVE'
+            };
+            const { enzymeWrapper } = todosSetup(props);
+            expect(enzymeWrapper.find('ul').children().nodes.length).toEqual(1);
+            expect(enzymeWrapper.find('ul li .todo-text').text()).toEqual('ddf');
         });
     });
 });
